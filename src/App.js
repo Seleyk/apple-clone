a286fffc0023e0db61f5875027a869777144156a
Steven Seley Jr <seleysteven2@gmail.com>
2024-03-23 16:57:06 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Highlights from './components/Highlights';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
+}
+
+export default App;
